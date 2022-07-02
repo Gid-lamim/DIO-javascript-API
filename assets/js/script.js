c3158af8716811f75ba0f80aece969cf06da6de3
Gid.Lamim <gid.lamim@gmail.com>
2022-07-01 23:00:50 -0300

    add script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-const BASE_URL = "https://thatcopy.pw/catapi/rest";
+const BASE_URL = "https://thatcopy.pw/catapi/rest/";
+const catBtn = document.getElementById("changePic");
+
 
 const getCats = async () => { // this is an arrow function 
-    try{
+    //try{
         const data = await fetch(BASE_URL);
         const json = await data.json();
+        console.log(json);
+        return json.weburl;
+    //} catch (e) {
+     //   console.log(e.message);
 
-        return json.webpurl;
-    } catch (e) {
-        console.log(e.message);
-
-    }
-
+    //}
 }
+const loadImg = async () =>{
+    const catImg = document.getElementById("catImg");
+    catImg.src = await getCats();
+    console.log(catImg.src);
+}
+
+
+catBtn.addEventListener('click', loadImg);
